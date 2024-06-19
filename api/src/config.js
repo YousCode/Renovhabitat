ca52dd29d0689606290ffdc8505bfe8a9526b6f9
Marty <martyrabord@yahoo.fr>
2024-06-19 17:50:46 +0200

    UPDATE

--- api/src/config.js
+++ api/src/config.js
@@ -4,6 +4,7 @@ const getEnvironment = () => process.env.ENVIRONMENT || "development";
 
 const config = {
   PORT: process.env.PORT || 8080,
+  // MONGO_URL: process.env.MONGO_URL || "mongodb+srv://martyrabord:a6OUq9xZdqid2ys9@bddrenovhabita.ydveh7j.mongodb.net/?retryWrites=true&w=majority&appName=bddRenovhabita",
   MONGO_URL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017/Renovhanbitat",
   secret: process.env.SECRET || "not-so-secret",
   APP_URL: process.env.APP_URL || "http://localhost:8082",
